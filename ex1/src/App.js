eb9885fa612da5d0bd6e3940a7f87fd683ebdb14
phiphongbee108 <phiphongbee108@gmail.com>
2022-02-22 15:40:10 +0700

    Components Part 2

--- ex1/src/App.js
+++ ex1/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <Menu />
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
