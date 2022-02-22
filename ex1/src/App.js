6455242c12a8c19b86528420d0a9645b8e619df8
phiphongbee108 <phiphongbee108@gmail.com>
2022-02-22 15:59:53 +0700

    Presentational and Container Components

--- ex1/src/App.js
+++ ex1/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
 
   render() {
     return (
-      <div>
-        <Menu dishes={this.state.dishes} />
+      <div className="App">
+        <Main />
       </div>
     );
   }
