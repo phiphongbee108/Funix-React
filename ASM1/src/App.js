7f35338b78a4e533dd922daf1a279bbdc281761c
phiphongbee108 <phiphongbee108@gmail.com>
2022-02-22 15:21:35 +0700

    Components Part 1

--- ASM1/src/App.js
+++ ASM1/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { DEPARTMENTS, ROLE, STAFFS } from './shared/staffs.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      staffs: STAFFS
+    }
+  }
+
+  render() {
+    const employees = this.state.staffs.map(e => {
+      return (
+        <div key={e.id} className='col-6'>
+          {e.name}
+        </div>
+      );
+    });
+    return (
+      <div className='container'>
+        <div className='row'>
+          {employees}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ ex1/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './components/MenuComponent';
+
+function App() {
+  return (
+    <Menu />
+  );
+}
+
+export default App;
