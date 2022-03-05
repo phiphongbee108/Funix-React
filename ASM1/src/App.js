25f0d5f87401ce3798c01bd21ea5a8711af883c0
phiphongbee108 <phiphongbee108@gmail.com>
2022-03-05 16:59:28 +0700

    delete import

--- ASM1/src/App.js
+++ ASM1/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import { DEPARTMENTS, ROLE, STAFFS } from './shared/staffs.jsx';
+import { STAFFS } from './shared/staffs.jsx';
 import StaffList from './components/StaffList.js';
 
 class App extends Component {
