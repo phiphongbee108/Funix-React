4cb66db258640084dff9eacc96b2368ac6960f5b
phiphongbee108 <phiphongbee108@gmail.com>
2022-03-05 17:04:14 +0700

    fix size show

--- ASM1/src/components/StaffList.js
+++ ASM1/src/components/StaffList.js
@@ -19,22 +19,26 @@ class StaffList extends Component {
     showStaff(staff) {
         if (staff != null) {
           return (
-            <div className="col-12">
-              <Card>
-                <CardImg width="100%" src={staff.image} alt={staff.name} />
-                <CardBody>
-                  <CardTitle>Họ và tên: {staff.name}</CardTitle>
-                  <CardText>
-                    Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
-                  </CardText>
-                  <CardText>
-                    Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-                  </CardText>
-                  <CardText>Phòng ban: {staff.department.name}</CardText>
-                  <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-                  <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-                </CardBody>
-              </Card>
+            <div className="row">
+              <div className="col-3"></div>
+              <div className="col-6">
+                <Card>
+                  <CardImg width="100%" src={staff.image} alt={staff.name} />
+                  <CardBody>
+                    <CardTitle>Họ và tên: {staff.name}</CardTitle>
+                    <CardText>
+                      Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
+                    </CardText>
+                    <CardText>
+                      Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
+                    </CardText>
+                    <CardText>Phòng ban: {staff.department.name}</CardText>
+                    <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+                    <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+                  </CardBody>
+                </Card>
+              </div>
+              <div className="col-3"></div>
             </div>
           );
         } else {
